4517005e7eb579910f3eecbc010626a955abdd73
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-11-24 21:58:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pacman -S qtile alacritty picom nitrogen neofetch gnome-themes-extra nvidia-settings thunar maim feh p7zip
+pacman -S qtile alacritty picom nitrogen neofetch gnome-themes-extra nvidia-settings thunar maim feh p7zip nerd-fonts starship
 cp .bashrc ~/.bashrc && cp .bash_profile ~/.bash_profile
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 cp .xinitrc ~/.xinitrc
@@ -9,3 +9,4 @@ cp .config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp .config/picom/picom.conf ~/.config/picom/picom.conf
 cp .config/qtile/config.py ~/.config/qtile/config.py && cp .config/qtile/autostart.sh ~/.config/qtile/autostart.sh
 chmod +x ~/.config/qtile/autostart.sh
+cp .images/* ~/Pictures
