f3d477b96a84bf008a63d0ff4841c7fae9fe2284
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-11-25 00:48:10 +0100

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -3,72 +3,107 @@ import subprocess
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 from libqtile.core.manager import Qtile
+from libqtile.dgroups import simple_key_binder
+
+
+# ----------------------------
+# -------- Hotkeys -----------
+# ----------------------------
 
 mod = "mod4"
 terminal = "alacritty"
 
 keys = [
+
+    # Misc hotkeys
+
     Key([mod], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    # Toggle between different layouts
-    Key([mod], "x", lazy.next_layout(), desc="Toggle between layouts"),
-    # Switch between windows
+
+    # Window hotkeys
+
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "c", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+
+    # App hotkeys
+
+    Key([mod], "e", lazy.spawn("thunar"), desc="Launch thunar"),
+    Key([mod], "t", lazy.spawn("teamspeak"), desc="Launch teamspeak"),
+    Key([mod], "s", lazy.spawn("steam"), desc="Launch steam"),
+    Key([mod], "d", lazy.spawn("discord"), desc="Launch discord"),
+    Key([mod], "b", lazy.spawn("firefox"), desc="Launch firefox"),
+
+    # Layout hotkeys
+
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "Tab", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows
+
     Key([mod, "control"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "control"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "control"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "control"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows
+
     Key([mod, "shift"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "shift"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "shift"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "shift"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Stack windows
+
+    Key([mod], "x", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "y", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Stacks windows"),
 ]
 
-groups = [Group(i) for i in "123456789"]
 
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-        ]
-    )
+# ----------------------------
+# ----- Groups & Layouts -----
+# ----------------------------
+
+groups = [
+    Group("󰖟", layout="max"),
+    Group("󰓓", spawn="steam"),
+    Group("󰙯", spawn=["discord", "teamspeak"]),
+    Group("", spawn=["keepassxc", "thunar"]),
+]
 
 layouts = [
     layout.Columns(border_normal="#1870df", border_focus="#ffcc44", border_focus_stack=["#008f39", "#7d9e7f"], border_width=2, margin=8),
     layout.Max(margin=12, border_focus="#ffcc44", border_width=2),
 ]
 
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+
+
+# ---------------------------
+# --------- Widgets ---------
+# ---------------------------
+
 widget_defaults = dict(
     font="sans",
-    fontsize=14,
+    fontsize=16,
     padding=12,
 )
 extension_defaults = widget_defaults.copy()
@@ -99,56 +134,26 @@ screens = [
 	    margin=[10, 10, 4, 10],
         ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
         # x11_drag_polling_rate = 60,
     ),
 ]
 
-# Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
-]
-
-dgroups_key_binder = None
+dgroups_key_binder = simple_key_binder("mod4")
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
+wmname = "Qtile"
 auto_minimize = True
 
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "Qtile"
+# ---------------------------
+# ---------- Hooks ----------
+# ---------------------------
 
 @hook.subscribe.startup_once
 def autostart():
