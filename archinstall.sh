186676cceb4be8878f9d5f6308acb9af522eb894
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-12-01 19:57:58 +0100

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -8,8 +8,9 @@ sudo systemctl enable lightdm.service
 # config: copying the .xinitrc config
 cp .xinitrc ~/.xinitrc
 
-# install: Qtile(WindowManager), Picom(Compositor), PipeWire-Pulse(Audio), Nitrogen(Background)
-yes | sudo pacman -S qtile picom pipewire-pulse nitrogen
+# install: Qtile(WindowManager), Picom(Compositor), PipeWire-Pulse(Audio), Nitrogen(Background), Python
+yes | sudo pacman -S qtile picom nitrogen python
+yes 1 | sudo pacman -S pipewire-pulse 
 # Creating directories and copying the Picom and Qtile configs, autostart script
 mkdir -p ~/.config/picom && mkdir -p ~/.config/qtile
 cp .config/picom/picom.conf ~/.config/picom/picom.conf
