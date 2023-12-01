b1a47c1ded2c6993cb6a4916379a00336dcea391
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-12-01 19:50:10 +0100

    Create archinstall.sh

---
+++ archinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# install: X11(DisplayServer), lightdm(DisplayManager), NVIDIA drivers, Nano(Editor)
+# yes | sudo pacman -S xorg xorg-init nvidia
+yes | sudo pacman -S lightdm nano
+# service: enabling the lightdm service
+sudo systemctl enable lightdm.service
+# config: copying the .xinitrc config
+cp .xinitrc ~/.xinitrc
+
+# install: Qtile(WindowManager), Picom(Compositor), PipeWire-Pulse(Audio), Nitrogen(Background)
+yes | sudo pacman -S qtile picom pipewire-pulse nitrogen
+# Creating directories and copying the Picom and Qtile configs, autostart script
+mkdir -p ~/.config/picom && mkdir -p ~/.config/qtile
+cp .config/picom/picom.conf ~/.config/picom/picom.conf
+cp .config/qtile/config.py ~/.config/qtile/config.py
+cp .config/qtile/autostart.sh ~/.config/qtile/autostart.sh
+chmod +x ~/.config/qtile/autostart.sh
+# Compiling the qtile config
+python -m py_compile ~/.config/qtile/config.py
+# Setting up the wallpaper
+cp .images/* ~/Pictures
+nitrogen ~/Pictures && nitrogen --restore
+
+
+
