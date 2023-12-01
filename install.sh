b663df15540c5770142614dcd0fcf4127cba58f6
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-12-02 00:02:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,80 @@
 #!/bin/sh
 
-# Installing packages
-pacman -S qtile pipewire-pulse arc-gtk-theme picom nitrogen alacritty nerd-fonts starship neofetch nvidia-settings maim feh p7zip 
+# ----- installing the base system
 
-# Copying the config files
-cp .bashrc ~/.bashrc && cp .bash_profile ~/.bash_profile
-cp .gtkrc-2.0 ~/.gtkrc-2.0
-cp .xinitrc ~/.xinitrc
-mkdir -p ~/.config/alacritty && mkdir -p ~/.config/gtk-3.0 && mkdir -p ~/.config/picom && mkdir -p ~/.config/qtile
-cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-cp .config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-cp .config/picom/picom.conf ~/.config/picom/picom.conf
-cp .config/qtile/config.py ~/.config/qtile/config.py && cp .config/qtile/autostart.sh ~/.config/qtile/autostart.sh
+# display server
+sudo pacman -S --noconfirm xorg xorg-init xorg-input base-devel
+# display manager
+sudo pacman -S --noconfirm lightdm
+# drivers GPU
+sudo pacman -S --noconfirm nvidia
+# window manager
+sudo pacman -S --noconfirm qtile
+# windo switcher
+sudo pacman -S --noconfirm rofi
+# X compositor
+sudo pacman -S --noconfirm picom
+# audio
+sudo pacman -S --noconfirm pipewire-pulse
+# wallpaper
+sudo pacman -S --noconfirm nitrogen
+# python
+sudo pacman -S --noconfirm python
+# python - psutil
+sudo pacman -S --noconfirm python-psutil
+# yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Y --gendb
+# font
+sudo pacman -S nerd-fonts
+# theme
+sudo pacman -S --noconfirm arc-gtk-theme
+# theme - icons
+sudo pacman -S --noconfirm papirus-icon-theme
+# theme - cursor
+yay -S qogir-cursor-theme-git
+# terminal
+sudo pacman -S --noconfirm alacritty
+# shell
+sudo pacman -S --noconfirm starship
+# neofetch
+sudo pacman -S --noconfirm neofetch
+# editor
+sudo pacman -S --noconfirm nano
+# file manager (terminal)
+sudo pacman -S --noconfirm ranger
+# file manager
+sudo pacman -S --noconfirm thunar
+
+# -----  configs and configuration
+
+# creating directories
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/picom
+mkdir -p ~/.config/qtile
+mkdir -p ~/Images
+
+# copying the configs
+cp ~/Github/arch/.bashrc ~/.bashrc 
+cp ~/Github/arch/.bash_profile ~/.bash_profile
+cp ~/Github/arch/.gtkrc-2.0 ~/.gtkrc-2.0
+cp ~/Github/arch/.xinitrc ~/.xinitrc
+cp ~/Github/arch/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ~/Github/arch/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp ~/Github/arch/.config/picom/picom.conf ~/.config/picom/picom.conf
+cp ~/Github/arch/.config/qtile/config.py ~/.config/qtile/config.py
+cp ~/Github/arch/.config/qtile/autostart.sh ~/.config/qtile/autostart.sh
 chmod +x ~/.config/qtile/autostart.sh
+cp .images/* ~/Images
 
-# Setting up the wallpaper
-cp .images/* ~/Pictures
-nitrogen ~/Pictures && nitrogen --restore
+# enable lightdm service
+sudo systemctl enable lightdm.service
 
-# Compiling the qtile config
+# compiling the qtile config
 python -m py_compile ~/.config/qtile/config.py
 
-# Installing software
-pacman -S thunar keepassxc discord steam thunderbird
+# setting up the wallpaper
+nitrogen ~/Images && nitrogen --restore
