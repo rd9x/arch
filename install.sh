58305a8f6315fe8bfb3e5e55642824fd6df5f82e
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-12-03 14:38:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,13 @@
 
 # ----- installing the base system
 
+# update
+sudo pacman -Syu
 # mirror ranking
 sudo pacman -S --noconfirm pacman-contrib
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sudo chmod a+w /etc/pacman.d/mirrorlist
 sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-# pacman config
-sudo cp ~/Github/arch/etc/pacman.conf /etc/pacman.conf
-sudo pacman -Sy
 # yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -31,41 +30,23 @@ sudo pacman -S --noconfirm pipewire-pulse
 sudo pacman -S --noconfirm alsa-utils
 # wallpaper
 sudo pacman -S --noconfirm nitrogen
-# python
-sudo pacman -S --noconfirm python
-# python - psutil
-sudo pacman -S --noconfirm python-psutil
-# font
-sudo pacman -S nerd-fonts
+# python, psutil
+sudo pacman -S --noconfirm python python-psutil
 # task manager
 sudo pacman -S --noconfirm htop
-# theme
+# theme, icons, cursor, font
 yay -S --noconfirm catppuccin-gtk-theme-macchiato
-# theme - icons
-sudo pacman -S --noconfirm papirus-icon-theme
-# theme - cursor
 yay -S --noconfirm qogir-cursor-theme-git
-# terminal
-sudo pacman -S --noconfirm kitty
-# shell
-sudo pacman -S --noconfirm starship
-# neofetch
-sudo pacman -S --noconfirm neofetch
+sudo pacman -S --noconfirm papirus-icon-theme
+sudo pacman -S nerd-fonts
+# terminal, shell, neofetch
+sudo pacman -S --noconfirm kitty starship neofetch
 # editor
 sudo pacman -S --noconfirm nano
-# file archiver
-sudo pacman -S --noconfirm p7zip
-# file manager (terminal)
-sudo pacman -S --noconfirm ranger
-# file manager
-sudo pacman -S --noconfirm thunar
-sudo pacman -S --noconfirm gvfs
-# Image - Capture
-sudo pacman -S --noconfirm maim
-# Image - Viewer
-sudo pacman -S --noconfirm feh
-# Video - Player/Recorder
-sudo pacman -S --noconfirm ffmpeg
+# file archiver, file manager
+sudo pacman -S --noconfirm p7zip ranger thunar gvfs
+# Image and Video Player, Screenshot
+sudo pacman -S --noconfirm maim feh ffmpeg
 
 # Software
 sudo pacman -S --noconfirm firefox
@@ -76,39 +57,19 @@ yay -S --noconfirm teamspeak
 sudo pacman -S --noconfirm gpick
 
 
-# -----  configs and configuration
-
-# creating directories
-mkdir -p ~/.config/fontconfig
-mkdir -p ~/.config/gtk-3.0
-mkdir -p ~/.config/kitty
-mkdir -p ~/.config/neofetch
-mkdir -p ~/.config/picom
-mkdir -p ~/.config/qtile
-mkdir -p ~/Images
+# -----  configs
 
 # copying the configs
-cp ~/Github/arch/.bashrc ~/.bashrc 
-cp ~/Github/arch/.bash_profile ~/.bash_profile
-cp ~/Github/arch/.gtkrc-2.0 ~/.gtkrc-2.0
-cp ~/Github/arch/.nanorc ~/.nanorc
-cp ~/Github/arch/.xinitrc ~/.xinitrc
-cp ~/Github/arch/.config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
-cp ~/Github/arch/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-cp ~/Github/arch/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-cp ~/Github/arch/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-cp ~/Github/arch/.config/picom/picom.conf ~/.config/picom/picom.conf
-cp ~/Github/arch/.config/starship.toml ~/.config/starship.toml
-cp ~/Github/arch/.config/qtile/config.py ~/.config/qtile/config.py
-cp ~/Github/arch/.config/qtile/autostart.sh ~/.config/qtile/autostart.sh
+cp -r ~/Github/arch/.config/* ~/.config/
+cp -r ~/Github/arch/.home/* ~/
 chmod +x ~/.config/qtile/autostart.sh
-cp ~/Github/arch/.images/* ~/Images
+cp ~/Github/arch/.images/* ~/Pictures
 
 # compiling the qtile config
 python -m py_compile ~/.config/qtile/config.py
 
 # setting up the wallpaper
-nitrogen ~/Images/ --random --set-scaled --save
+nitrogen ~/Pictures/ --random --set-scaled --save
 nitrogen --restore
 
 # creating xorg.conf
