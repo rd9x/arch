39e2187adf4b31a647aff72bba4ba802989039ee
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2023-12-05 17:25:35 +0100

    Create mouse.sh

---
+++ mouse.sh
@@ -0,0 +1,3 @@
+xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 13 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 22 'libinput Accel Profile Enabled' 0, 1, 0
