2a56483a36816994344de4d4860c01b5d781bdcd
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-01-09 22:02:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo pacman -S --noconfirm python python-psutil
 # wallpaper, theme, icons, cursor, font
 yay -S --noconfirm catppuccin-gtk-theme-macchiato
 yay -S --noconfirm qogir-cursor-theme-git
-sudo pacman -S --noconfirm nitrogen papirus-icon-theme breeze breeze-gtk
+sudo pacman -S --noconfirm nitrogen papirus-icon-theme breeze breeze-gtk noto-fonts-emoji
 sudo pacman -S nerd-fonts
 # task manager
 sudo pacman -S --noconfirm htop
