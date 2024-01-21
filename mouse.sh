e557d0400e62404e91557c6e32490610d60ed2d8
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-01-21 18:01:51 +0100

    Delete mouse.sh

--- mouse.sh
+++
@@ -1,3 +0,0 @@
-xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1, 0
-xinput --set-prop 13 'libinput Accel Profile Enabled' 0, 1, 0
-xinput --set-prop 22 'libinput Accel Profile Enabled' 0, 1, 0
