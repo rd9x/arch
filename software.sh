ad9af0893bdc37679417e98e967fc0461aa086d9
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-04 00:05:21 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -3,24 +3,34 @@
 # ---------- Software Installation ----------
 
 # Discord
-# sudo pacman -S --noconfirm discord
+sudo pacman -S --noconfirm discord
 
 # Gimp
 sudo pacman -S --noconfirm gimp
 
-# QTPass/KeepassXC + Kleopatra
-sudo pacman -S --noconfirm qtpass keepassxc
-sudo pacman -S --noconfirm kleopatra
+# KeepassXC
+sudo pacman -S --noconfirm keepassxc
 
 # Steam, Gamemode, Mangohud
 sudo pacman -S steam
 sudo pacman -S --noconfirm gamemode mangohud
 
-# Teamspeak
-yay -S --noconfirm teamspeak
-
 # Thunderbird
 sudo pacman -S --noconfirm thunderbird
 
+# Teamspeak
+yay -S --noconfirm teamspeak
+
 # MullvadVPN
 yay -S mullvad-vpn
+
+# ---------- Archiv ----------
+
+# ProtonVPN / OpenVPN
+# sudo pacman -S --noconfirm wget openvpn openresolv
+# sudo wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
+# sudo chmod +x "/etc/openvpn/update-resolv-conf"
+
+# QTPass + Kleopatra
+# sudo pacman -S --noconfirm qtpass 
+# sudo pacman -S --noconfirm kleopatra
