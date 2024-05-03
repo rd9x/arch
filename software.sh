51c0f04cd137124611f50f2d013f8fa7c1972ba3
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-04 00:03:16 +0200

    Create software.sh

---
+++ software.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# ---------- Software Installation ----------
+
+# Discord
+# sudo pacman -S --noconfirm discord
+
+# Gimp
+sudo pacman -S --noconfirm gimp
+
+# QTPass/KeepassXC + Kleopatra
+sudo pacman -S --noconfirm qtpass keepassxc
+sudo pacman -S --noconfirm kleopatra
+
+# Steam, Gamemode, Mangohud
+sudo pacman -S steam
+sudo pacman -S --noconfirm gamemode mangohud
+
+# Teamspeak
+yay -S --noconfirm teamspeak
+
+# Thunderbird
+sudo pacman -S --noconfirm thunderbird
+
+# MullvadVPN
+yay -S mullvad-vpn
