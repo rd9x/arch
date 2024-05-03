86307c475812fac6e9a589905c8232d0724ba629
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-03 23:17:57 +0200

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -19,3 +19,10 @@ sudo pacman -S --noconfirm breeze breeze-gtk
 
 # ----- Configuration Files
 
+cp ~/Github/arch/etc/environment /etc/environment
+cp ~/Github/arch/home/.gtkrc-2.0 ~/.gtkrc-2.0
+cp ~/Github/arch/home/.profile ~/.profile
+cp ~/Github/arch/home/config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+cp ~/Github/arch/home/config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp ~/Github/arch/home/config/qt5ct/colors/catppuccin-macchiato.conf ~/.config/qt5ct/colors/catppuccin-macchiato.conf
+cp ~/Github/arch/home/config/qt5ct/qt5ct.conf ~/.config/qt5ct/qt5ct.conf
