b173fa425567b503381c7e060d00e3798cb99725
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-04 12:13:06 +0200

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -3,26 +3,25 @@
 # ---------- Theme ----------
 
 # cursor 
-yay -S --noconfirm qogir-cursor-theme-git
+yay -S --noconfirm --needed qogir-cursor-theme-git
 
 # font + emojis
-sudo pacman -S nerd-fonts
-sudo pacman -S --noconfirm noto-fonts-emoji
+sudo pacman -S --needed nerd-fonts
+sudo pacman -S --noconfirm --needed noto-fonts-emoji
 
 # icons
-sudo pacman -S --noconfirm papirus-icon-theme
+sudo pacman -S --noconfirm --needed papirus-icon-theme
 
 # system theme
-yay -S --noconfirm catppuccin-gtk-theme-macchiato
-sudo pacman -S --noconfirm breeze breeze-gtk
+yay -S --noconfirm --needed catppuccin-gtk-theme-macchiato
+sudo pacman -S --noconfirm --needed breeze breeze-gtk
 
 
 # ----- Configuration Files
 
-cp ~/Github/arch/etc/environment /etc/environment
-cp ~/Github/arch/home/.gtkrc-2.0 ~/.gtkrc-2.0
-cp ~/Github/arch/home/.profile ~/.profile
-cp ~/Github/arch/home/config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
-cp ~/Github/arch/home/config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-cp ~/Github/arch/home/config/qt5ct/colors/catppuccin-macchiato.conf ~/.config/qt5ct/colors/catppuccin-macchiato.conf
-cp ~/Github/arch/home/config/qt5ct/qt5ct.conf ~/.config/qt5ct/qt5ct.conf
+rsync ~/Github/arch/home/.gtkrc-2.0 ~/
+rsync ~/Github/arch/home/.profile ~/
+rsync -r ~/Github/arch/home/config/fontconfig/ ~/.config/fontconfig
+rsync -r ~/Github/arch/home/config/gtk-3.0/ ~/.config/gtk-3.0
+rsync -r ~/Github/arch/home/config/qt5ct/ ~/.config/qt5ct
+sudo rsync ~/Github/arch/etc/environment /etc
