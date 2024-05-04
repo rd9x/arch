2dac399559c7562bf8794a36913170c3d2644336
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-04 12:09:33 +0200

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -3,30 +3,30 @@
 # ---------- System Utilities and Tools ----------
 
 # terminal, shell, editor, neofetch
-sudo pacman -S --noconfirm kitty starship nano neofetch
+sudo pacman -S --noconfirm --needed kitty starship nano neofetch
 
 # task manager
-sudo pacman -S --noconfirm htop
+sudo pacman -S --noconfirm --needed htop
 
 # python, psutil
-sudo pacman -S --noconfirm python python-psutil
+sudo pacman -S --noconfirm --needed python python-psutil
 
 # browser
-sudo pacman -S --noconfirm firefox
+sudo pacman -S --noconfirm --needed firefox
 
 # file manager + 7zip/unrar
-sudo pacman -S --noconfirm thunar gvfs xfce4-settings
-sudo pacman -S --noconfirm ranger ncdu
-sudo pacman -S --noconfirm p7zip unrar
+sudo pacman -S --noconfirm --needed thunar gvfs xfce4-settings
+sudo pacman -S --noconfirm --needed ranger ncdu
+sudo pacman -S --noconfirm --needed p7zip unrar
 
 # multimedia (image, screenshot, video) + color picker
-sudo pacman -S --noconfirm feh maim ffmpeg gpick
+sudo pacman -S --noconfirm --needed feh maim ffmpeg gpick
 
 # ----- Configuration Files
 
-cp ~/Github/arch/home/config/htop/htoprc ~/.config/htop/htoprc
-cp ~/Github/arch/home/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-cp ~/Github/arch/home/config/neofetch/config.conf ~/.config/neofetch/config.conf
-cp ~/Github/arch/home/config/starship.toml ~/.config/starship.toml
-cp ~/Github/arch/home/.nanorc ~/.nanorc
+rsync -r ~/Github/arch/home/config/htop/ ~/.config/htop
+rsync -r ~/Github/arch/home/config/kitty/ ~/.config/kitty
+rsync -r ~/Github/arch/home/config/neofetch/ ~/.config/neofetch
+rsync ~/Github/arch/home/config/starship.toml ~/.config
+rsync ~/Github/arch/home/.nanorc ~/
 
