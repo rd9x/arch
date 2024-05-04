3213011eaf21759cb24c7c2b10e0ce6a9d2f2963
Rudi9x <134175623+Rudi9x@users.noreply.github.com>
2024-05-04 12:07:15 +0200

    Update display.sh

--- display.sh
+++ display.sh
@@ -14,44 +14,43 @@ yay -Y --gendb
 # ----- Audio and Display
 
 # audio
-sudo pacman -S --noconfirm pipewire-pulse alsa-utils pavucontrol
+sudo pacman -S --noconfirm --needed pipewire-pulse alsa-utils pavucontrol
 
 # display server 
-sudo pacman -S --noconfirm base-devel xorg xorg-xinit xorg-xinput
+sudo pacman -S --noconfirm --needed base-devel xorg xorg-xinit xorg-xinput
 
 # GPU driver
-sudo pacman -S --noconfirm nvidia-dkms nvidia-settings nvidia-utils lib32-nvidia-utils
-sudo pacman -S --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S --noconfirm --needed nvidia-dkms nvidia-settings nvidia-utils lib32-nvidia-utils
+sudo pacman -S --noconfirm --needed vulkan-icd-loader lib32-vulkan-icd-loader
 
 # compositor
-sudo pacman -S --noconfirm picom 
+sudo pacman -S --noconfirm --needed picom 
 
 # window manager + switcher
-sudo pacman -S --noconfirm rofi
-yay -S --noconfirm qtile-git qtile-extras-git
+sudo pacman -S --noconfirm --needed rofi
+yay -S --noconfirm --needed qtile-git qtile-extras-git
 
 # wallpaper
-sudo pacman -S --noconfirm nitrogen
+sudo pacman -S --noconfirm --needed nitrogen
 
 
 # ----- Configuration
 
 # .files
-cp ~/Github/arch/home/.asoundrc ~/.asoundrc
-cp ~/Github/arch/home/.bashrc ~/.bashrc
-cp ~/Github/arch/home/.bash_profile ~/.bash_profile
-cp ~/Github/arch/home/.xinitrc ~/.xinitrc
+rsync ~/Github/arch/home/.asoundrc ~/
+rsync ~/Github/arch/home/.bashrc ~/
+rsync ~/Github/arch/home/.bash_profile ~/
+rsync ~/Github/arch/home/.xinitrc ~/
 
 # qtile and picom
-cp ~/Github/arch/home/config/picom/picom.conf ~/.config/picom/picom.conf
-cp ~/Github/arch/home/config/qtile/autostart.sh ~/.config/qtile/autostart.sh
-cp ~/Github/arch/home/config/qtile/config.py ~/.config/qtile/config.py
-cp ~/Github/arch/home/config/rofi/config.rasi ~/.config/rofi/config.rasi
+rsync -r ~/Github/arch/home/config/picom/ ~/.config/picom
+rsync -r ~/Github/arch/home/config/qtile/ ~/.config/qtile
+rsync -r ~/Github/arch/home/config/rofi/ ~/.config/rofi
 chmod +x ~/.config/qtile/autostart.sh
 python -m py_compile ~/.config/qtile/config.py
 
 # setting up the wallpaper
-cp -r ~/Github/arch/Pictures ~/Pictures
+rsync -r ~/Github/arch/Pictures/ ~/Pictures
 nitrogen ~/Pictures/ --random --set-scaled --save
 nitrogen --restore
 
