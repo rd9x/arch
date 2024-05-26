78cb58bcb6de13c39ce5653ff17c231c51c0d508
rd9x <134175623+rd9x@users.noreply.github.com>
2024-05-26 14:55:25 +0200

    Update 01_audio_display.sh

--- 01_audio_display.sh
+++ 01_audio_display.sh
@@ -10,6 +10,8 @@ cd ~/Github && git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 yay -Y --gendb
 
+# sddm theme
+cd ~/Github && git clone https://github.com/rd9x/aerial-sddm-theme
 
 # ----- Audio and Display
 
@@ -19,12 +21,15 @@ sudo pacman -S --noconfirm --needed pipewire-pulse alsa-utils pavucontrol
 # display server 
 sudo pacman -S --noconfirm --needed base-devel xorg xorg-xinit xorg-xinput
 
+# compositor
+sudo pacman -S --noconfirm --needed picom 
+
+# login manager
+sudo pacman -S --noconfirm --needed sddm
+
 # GPU driver
 sudo pacman -S --noconfirm --needed nvidia nvidia-settings nvidia-utils lib32-nvidia-utils
 sudo pacman -S --noconfirm --needed vulkan-icd-loader lib32-vulkan-icd-loader
-
-# compositor
-sudo pacman -S --noconfirm --needed picom 
 
 # window manager + switcher
 sudo pacman -S --noconfirm --needed rofi
@@ -55,6 +60,10 @@ python -m py_compile ~/.config/qtile/config.py
 rsync -r ~/Github/arch/Pictures/ ~/Pictures
 nitrogen ~/Pictures/ --random --set-scaled --save
 nitrogen --restore
+
+# sddm theme and enabling service
+rsync -r ~/Github/aerial-sddm-theme/ /usr/share/sddm/themes/rd9x
+sudo systemctl enable sddm.service
 
 # creating xorg.conf and start X11
 sudo nvidia-xconfig
