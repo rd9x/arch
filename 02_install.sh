0a05c15e66de2bdeaaff2a379c18b73ad7a871ef
rd9x <134175623+rd9x@users.noreply.github.com>
2024-06-23 00:28:28 +0200

    Create 02_install.sh

---
+++ 02_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# gnome extensions
+gext install 1460 # vitals
+gext install 2986 # runcat
+gext install 3193 # blur my shell
+gext install 3843 # just perfection
+gext install 4679 # burn my windows
+gext install 5389 # screen rotate
