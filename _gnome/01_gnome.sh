00da19f06d7d8055c21838701c0cff893ff97207
rd9x <134175623+rd9x@users.noreply.github.com>
2024-06-23 00:25:41 +0200

    Delete _gnome/01_gnome.sh

--- _gnome/01_gnome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# optional
-# yay -S --noconfirm --needed pamac-aur
-# sudo pacman -S --noconfirm --needed gnome-software flatpak
-
-# webcam
-# sudo pacman -S --noconfirm --needed snapshot
