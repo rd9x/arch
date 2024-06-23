74ad0adf3b31ebaa7c287f6e62397a95ef61ef24
rd9x <134175623+rd9x@users.noreply.github.com>
2024-06-23 02:50:53 +0200

    Update 02_install.sh

--- 02_install.sh
+++ 02_install.sh
@@ -8,13 +8,17 @@ gext install 3843 # just perfection
 gext install 4679 # burn my windows
 gext install 5389 # screen rotate
 
-gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true &> /dev/null
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close" &> /dev/null
-gsettings set org.gnome.mutter center-new-windows true &> /dev/null
-gsettings set org.gnome.desktop.wm.preferences focus-mode sloppy &> /dev/null
-gsettings set org.gnome.desktop.wm.preferences auto-raise true &> /dev/null
-gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-dark-cursors' &> /dev/null
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &> /dev/null
-gsettings set org.gnome.desktop.interface document-font-name 'Comfortaa 11' &> /dev/null
-gsettings set org.gnome.desktop.interface font-name 'Comfortaa 11' &> /dev/null
-gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat &> /dev/null
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gsettings set org.gnome.mutter center-new-windows true
+gsettings set org.gnome.desktop.wm.preferences focus-mode sloppy
+gsettings set org.gnome.desktop.wm.preferences auto-raise true
+gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-dark-cursors'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface document-font-name 'Comfortaa 11'
+gsettings set org.gnome.desktop.interface font-name 'Comfortaa 11'
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
+
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/Github/arch/dconf/kb_media
+dconf load /org/gnome/shell/keybindings/ < ~/Github/arch/dconf/kb_shell
+dconf load /org/gnome/desktop/wm/keybindings/ < ~/Github/arch/dconf/kb_wm
