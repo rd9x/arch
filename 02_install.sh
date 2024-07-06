9e708d21a7f6a545aff1dae47e3d96e1bc9d530e
rd9x <134175623+rd9x@users.noreply.github.com>
2024-07-06 16:56:58 +0200

    Update 02_install.sh

--- 02_install.sh
+++ 02_install.sh
@@ -5,6 +5,7 @@ gext install 1460 # vitals
 gext install 2986 # runcat
 gext install 3193 # blur my shell
 gext install 3843 # just perfection
+gext install 4481 # forge
 gext install 4679 # burn my windows
 gext install 5389 # screen rotate
 
