41dccc5f3394ee7056e6b570e8d8cc6e34501bc8
rd9x <134175623+rd9x@users.noreply.github.com>
2025-10-29 21:07:52 +0100

    Update autostart.sh

--- home/config/qtile/autostart.sh
+++ home/config/qtile/autostart.sh
@@ -3,7 +3,13 @@
 nitrogen --restore &
 picom -b
 xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 10 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 11 'libinput Accel Profile Enabled' 0, 1, 0
 xinput --set-prop 13 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 16 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 17 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 20 'libinput Accel Profile Enabled' 0, 1, 0
+xinput --set-prop 21 'libinput Accel Profile Enabled' 0, 1, 0
 xinput --set-prop 22 'libinput Accel Profile Enabled' 0, 1, 0
 
 # set compose key
